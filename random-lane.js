e40eb20eee477798c0ff6e4941c04626f09e2c8c
kohei.okamoto <59508678+okaponta@users.noreply.github.com>
2023-11-15 12:57:33 +0900

    Create random-lane.js

---
+++ random-lane.js
@@ -0,0 +1,13 @@
+javascript:(function() {
+  var targetClass = 'inner';
+  var elements = document.querySelectorAll('.' + targetClass + 'li:first-child');
+
+  for (var i = 0; i < elements.length; i++) {
+    var text = elements[i].textContent.trim();
+    console.log(text);
+    var modifiedText = text.replace(/[1357]/g, function(match) {
+      return '<span style="color:red;">' + match + '</span>';
+    });
+    elements[i].innerHTML = modifiedText;
+  }
+})();
